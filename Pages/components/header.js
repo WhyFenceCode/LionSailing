63b341095b0eabbcf9967eda6410d705c7769b98
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-09-26 13:20:52 -0700

    Update header.js

--- Pages/components/header.js
+++ Pages/components/header.js
@@ -1,36 +1,7 @@
 const headerTemplate = document.createElement('template');
 
 headerTemplate.innerHTML = `
-  <style>
-    nav {
-      height: 40px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      background-color:  #0a0a23;
-    }
-
-    ul {
-      padding: 0;
-    }
-    
-    ul li {
-      list-style: none;
-      display: inline;
-    }
-    
-    a {
-      font-weight: 700;
-      margin: 0 25px;
-      color: #fff;
-      text-decoration: none;
-    }
-    
-    a:hover {
-      padding-bottom: 5px;
-      box-shadow: inset 0 -2px 0 0 #fff;
-    }
-  </style>
+  <link rel="stylesheet" href="header.css">
   <header>
     <nav>
       <ul>
